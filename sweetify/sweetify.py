79674e01643023983aae9630dbff7f336b42c0be
AshwinNS <ashwinashone@gmail.com>
2021-11-02 01:06:54 +0530

    update type to icon

--- sweetify/sweetify.py
+++ sweetify/sweetify.py
@@ -86,22 +86,22 @@ def toast(request, title, icon, **kwargs):
 
 
 def info(request, title, **kwargs):
-    kwargs['type'] = 'info'
+    kwargs['icon'] = 'info'
     return sweetalert(request, title, **kwargs)
 
 
 def success(request, title, **kwargs):
-    kwargs['type'] = 'success'
+    kwargs['icon'] = 'success'
     return sweetalert(request, title, **kwargs)
 
 
 def error(request, title, **kwargs):
-    kwargs['type'] = 'error'
+    kwargs['icon'] = 'error'
     return sweetalert(request, title, **kwargs)
 
 
 def warning(request, title, **kwargs):
-    kwargs['type'] = 'warning'
+    kwargs['icon'] = 'warning'
     return sweetalert(request, title, **kwargs)
 
 
