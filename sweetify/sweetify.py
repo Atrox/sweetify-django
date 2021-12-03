1741cc225387afe37ae74013a3b98ea63519a1c1
Atrox <hello@atrox.dev>
2021-12-03 20:39:25 +0100

    remove python 2 support, use black formatter

--- sweetify/sweetify.py
+++ sweetify/sweetify.py
@@ -1,107 +1,98 @@
 import json
-import sys
 
 from django.conf import settings
 
 from .encoder import LazyEncoder
 
 DEFAULT_OPTS = {
-    'showConfirmButton': False,
-    'timer': 2500,
-    'allowOutsideClick': True,
-    'confirmButtonText': 'OK',
+    "showConfirmButton": False,
+    "timer": 2500,
+    "allowOutsideClick": True,
+    "confirmButtonText": "OK",
 }
 
 
 def _flash_config(request, opts):
-    request.session['sweetify'] = json.dumps(opts, cls=LazyEncoder)
+    request.session["sweetify"] = json.dumps(opts, cls=LazyEncoder)
 
 
 def _flash_multiple_configs(request, jsonData):
-    request.session['sweetify'] = jsonData
-
-
-def _is_string(s):
-    # if we use Python 3
-    if sys.version_info[0] >= 3:
-        return isinstance(s, str)
-    # we use Python 2
-    return isinstance(s, basestring)
+    request.session["sweetify"] = jsonData
 
 
 def _treat_data(opts):
 
-    button = opts.pop('button', None)
+    button = opts.pop("button", None)
     if button:
-        opts['showConfirmButton'] = True
+        opts["showConfirmButton"] = True
 
-        if _is_string(button):
-            opts['confirmButtonText'] = button
+        if isinstance(button, str):
+            opts["confirmButtonText"] = button
 
-    persistent_toast = opts.pop('persistent_toast', None)
+    persistent_toast = opts.pop("persistent_toast", None)
     if persistent_toast:
-        opts['showConfirmButton'] = True
-        opts['timer'] = None
-        if _is_string(persistent_toast):
-            opts['confirmButtonText'] = persistent_toast    
+        opts["showConfirmButton"] = True
+        opts["timer"] = None
+        if isinstance(persistent_toast, str):
+            opts["confirmButtonText"] = persistent_toast
 
-    persistent = opts.pop('persistent', None)
+    persistent = opts.pop("persistent", None)
     if persistent:
-        opts['showConfirmButton'] = True
-        opts['allowOutsideClick'] = False
-        opts['timer'] = None
+        opts["showConfirmButton"] = True
+        opts["allowOutsideClick"] = False
+        opts["timer"] = None
 
-        if _is_string(persistent):
-            opts['confirmButtonText'] = persistent
+        if isinstance(persistent, str):
+            opts["confirmButtonText"] = persistent
 
     # sweetalert changes
-    if getattr(settings, 'SWEETIFY_SWEETALERT_LIBRARY', 'sweetalert2') == 'sweetalert':
-        opts['icon'] = opts.pop('type', None)
-        opts['closeOnClickOutside'] = opts.pop('allowOutsideClick', None)
+    if getattr(settings, "SWEETIFY_SWEETALERT_LIBRARY", "sweetalert2") == "sweetalert":
+        opts["icon"] = opts.pop("type", None)
+        opts["closeOnClickOutside"] = opts.pop("allowOutsideClick", None)
 
-        if opts.pop('showConfirmButton', False):
-            opts['button'] = opts['confirmButtonText']
+        if opts.pop("showConfirmButton", False):
+            opts["button"] = opts["confirmButtonText"]
         else:
-            opts['button'] = False
+            opts["button"] = False
     return opts
 
 
 def sweetalert(request, title, **kwargs):
     opts = DEFAULT_OPTS.copy()
     opts.update(kwargs)
-    opts['title'] = title
-    if opts.pop('is_toast', None):
-        opts.pop('allowOutsideClick', None)
+    opts["title"] = title
+    if opts.pop("is_toast", None):
+        opts.pop("allowOutsideClick", None)
     opts = _treat_data(opts)
     _flash_config(request, opts)
 
 
-def toast(request, title, icon='success', **kwargs):
-    kwargs['icon'] = icon
-    kwargs['toast'] = True
-    kwargs['position'] ='top-end'
-    kwargs['timerProgressBar'] = True
-    kwargs['is_toast'] = True
+def toast(request, title, icon="success", **kwargs):
+    kwargs["icon"] = icon
+    kwargs["toast"] = True
+    kwargs["position"] = "top-end"
+    kwargs["timerProgressBar"] = True
+    kwargs["is_toast"] = True
     return sweetalert(request, title, **kwargs)
 
 
 def info(request, title, **kwargs):
-    kwargs['icon'] = 'info'
+    kwargs["icon"] = "info"
     return sweetalert(request, title, **kwargs)
 
 
 def success(request, title, **kwargs):
-    kwargs['icon'] = 'success'
+    kwargs["icon"] = "success"
     return sweetalert(request, title, **kwargs)
 
 
 def error(request, title, **kwargs):
-    kwargs['icon'] = 'error'
+    kwargs["icon"] = "error"
     return sweetalert(request, title, **kwargs)
 
 
 def warning(request, title, **kwargs):
-    kwargs['icon'] = 'warning'
+    kwargs["icon"] = "warning"
     return sweetalert(request, title, **kwargs)
 
 
--- sweetify/templatetags/sweetify.py
+++ sweetify/templatetags/sweetify.py
@@ -7,33 +7,37 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def sweetify(context):
-	opts = context.request.session.pop('sweetify', None)
+    opts = context.request.session.pop("sweetify", None)
+    library = getattr(settings, "SWEETIFY_SWEETALERT_LIBRARY", "sweetalert2")
 
-	if not opts:
-		return ''
-	if isinstance(opts, list):
-		if getattr(settings, 'SWEETIFY_SWEETALERT_LIBRARY', 'sweetalert2') == 'sweetalert':
-			#TODO Add sweetalert 1 compatibility with multiple alerts.
-			return''
-		script = concatenate(opts)
-	else:
-		if getattr(settings, 'SWEETIFY_SWEETALERT_LIBRARY', 'sweetalert2') == 'sweetalert2':
-			script = 'Swal.fire({})'.format(opts)
-		else:
-			script = 'swal({})'.format(opts)
-	return mark_safe("""<script>{}</script>""".format(script))
+    if not opts:
+        return ""
+
+    if isinstance(opts, list):
+        if library == "sweetalert":
+            raise RuntimeError("multiple alerts are currently not supported for sweetalert 1")
+
+        script = concatenate(opts)
+    else:
+        if library == "sweetalert2":
+            script = "Swal.fire({})".format(opts)
+        else:
+            script = "swal({})".format(opts)
+
+    return mark_safe("""<script>{}</script>""".format(script))
+
 
 def concatenate(list):
-	i = 0
-	length = len(list)
-	script = 'Swal.fire({})'
-	for opts in list:
-		if i == 0:
-			script = script.format(opts)
-			i = i + 1
-		elif i < length:
-			script += '.then((result) => {{if (result.value) {{Swal.fire({}'.format(opts)	
-			script += ')'
-	for k in range(length - 1):
-		script += '}})'
-	return script
+    i = 0
+    length = len(list)
+    script = "Swal.fire({})"
+    for opts in list:
+        if i == 0:
+            script = script.format(opts)
+            i = i + 1
+        elif i < length:
+            script += ".then((result) => {{if (result.value) {{Swal.fire({}".format(opts)
+            script += ")"
+    for k in range(length - 1):
+        script += "}})"
+    return script
